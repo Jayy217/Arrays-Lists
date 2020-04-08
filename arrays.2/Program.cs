6f5b1f738ce370ceee1ecb63c4d3e4a201b3e8e2
Jacob Youssif <103043338@student.swin.edu.au>
2020-04-08 18:39:24 +1000

    First

---
+++ arrays.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace arrays._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = {34,5,67,1,99,34,44,78,34,0};
+            int total = 0;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                total += values[i];
+            }
+
+            System.Console.WriteLine("The total of all values is " + total);
+        }
+    }
+}
