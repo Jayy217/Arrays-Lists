833465cdb3504b063fccf730ba58377d98c0731e
Jacob Youssif <103043338@student.swin.edu.au>
2020-04-09 09:59:47 +1000

    last 2 got me gud..

---
+++ lists.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>();
+            string input = "i";
+
+            while (input != "stop")
+            {
+                input = Console.ReadLine();
+                words.Add(input);
+            }
+
+            System.Console.WriteLine("----------------");
+            
+            for (int i = words.Count - 1; i >= 0; i--)
+            {
+                System.Console.WriteLine(words[i]);
+            }
+        }
+    }
+}
