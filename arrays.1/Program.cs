1199f08566592cae6f4bd3667e645b25b0754329
Jacob Youssif <103043338@student.swin.edu.au>
2020-04-08 16:57:57 +1000

    is this efficient?

---
+++ arrays.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace arrays._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string [5];
+            int i = 0;
+
+            System.Console.WriteLine("Please enter 5 names.");
+
+            while (i < names.Length)
+            {
+                names[i] = Console.ReadLine ();
+                i++;
+            }
+            i = 0;
+            while (i < names.Length)
+            {
+                System.Console.WriteLine("Hello " + names[i]);
+                i++;
+            }
+
+        }
+    }
+}
